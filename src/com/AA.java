52ef1739cb8f036a0db181d0744b02bb32eed13f
stefan <1693625254@qq.com>
2021-06-16 11:01:04 +0800

    java泛型

--- src/com/AA.java
+++ src/com/AA.java
@@ -1,7 +1,9 @@
 package com;
 
 import javax.jnlp.IntegrationService;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.function.Consumer;
 
 /**
@@ -20,9 +22,15 @@ public class AA {
         ArrayList<Integer> ts = new ArrayList<Integer>();
         ts.add(12);
         ts.add(123);
-        System.out.println(ts.toArray());
+//        System.out.println(ts.toArray());
 
 //        System.out.printf(k == j); //false
+
+        Date date = new Date();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        System.out.println("键盘充电完成时间： "+sdf.format(date));;
 
     }
 }
--- src/com/java/java_generics.java
+++ src/com/java/java_generics.java
@@ -1,5 +1,10 @@
 package com.java;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Zhang
  * @date 6/15/2021 1:25 PM
@@ -7,5 +12,22 @@ package com.java;
  */
 public class java_generics {
 
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+
+        List<Integer> list = new ArrayList<>();
+        list.add(12);
+//        list.add("asdf");   这里直接添加会报错
+        Class<? extends List> clazz = list.getClass();
+        Method add = clazz.getDeclaredMethod("add", Object.class);
+    // 通过反射添加，是可以的
+        add.invoke(list, "adsda");
+        System.out.println(list);
+
+
+    }
+
+    class Generic<T>{
+
+    }
 
 }
