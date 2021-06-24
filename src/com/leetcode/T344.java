0331697f67095a5e55231b07c2821a759191630c
stefan <1693625254@qq.com>
2021-06-24 13:22:10 +0800

    T344字符串反转

---
+++ src/com/leetcode/T344.java
@@ -0,0 +1,20 @@
+package com.leetcode;
+
+/**
+ * @author Zhang
+ * @date 6/24/2021 11:05 AM
+ * @description
+ *  字符串反转
+ */
+public class T344 {
+    public void reverseString(char[] s) {
+        int left = 0, right = s.length - 1;
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
