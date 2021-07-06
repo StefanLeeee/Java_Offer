9a15690eed9b8e3cca6861cd534668354a910e4f
rex <13545687855@163.com>
2021-07-06 12:17:04 +0800

    init

--- src/com/leetcode/T209.java
+++ src/com/leetcode/T209.java
@@ -1,6 +1,7 @@
 package com.leetcode;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class T209 {
     public static int minSubArrayLen(int s, int[] nums) {
@@ -51,6 +52,28 @@ public class T209 {
     }
 
 
+    class Solution1 {
+        public int minSubArrayLen(int s, int[] nums) {
+            int n = nums.length;
+            if (n == 0) {
+                return 0;
+            }
+            int ans = Integer.MAX_VALUE;
+            int start = 0, end = 0;
+            int sum = 0;
+            while (end < n) {
+                sum += nums[end];
+                while (sum >= s) {
+                    ans = Math.min(ans, end - start + 1);
+                    sum -= nums[start];
+                    start++;
+                }
+                end++;
+            }
+            return ans == Integer.MAX_VALUE ? 0 : ans;
+        }
+    }
+
 
     public static void main(String[] args) {
         StringBuffer stringBuffer = new StringBuffer();
@@ -58,6 +81,7 @@ public class T209 {
         int[] nums = new int[]{1, 4, 4};
         int i = minSubArrayLen(4, nums);
         System.out.print(i);
+        HashSet<?> hashSet = new HashSet<>();
 
     }
 
---
+++ src/com/shangguigu/java/mutiThread/Thread_Runnable.java
@@ -0,0 +1,9 @@
+package com.shangguigu.java.mutiThread;
+
+/**
+ * @author Zhang
+ * @date 7/6/2021 12:16 PM
+ * @description
+ */
+public class Thread_Runnable {
+}
