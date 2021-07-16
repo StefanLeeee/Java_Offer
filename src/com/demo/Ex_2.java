0f13544a38aa11f06a5ae199dae0e4ad1f5bcba2
rex <13545687855@163.com>
2021-07-16 16:41:19 +0800

    fix some

--- src/com/demo/Ex_2.java
+++ src/com/demo/Ex_2.java
@@ -6,7 +6,7 @@ public class Ex_2 {
 //    1. 从键盘循环输入正整数，当输入-1时结束，统计输入的正整数的个数。
 public static void main(String[] args) {
 
-    Scanner input = new Scanner(System.in);
+//    Scanner input = new Scanner(System.in);
     int a,b=1;
     int count=0;
     System.out.println("请输入正整数，输入-1时结束");
