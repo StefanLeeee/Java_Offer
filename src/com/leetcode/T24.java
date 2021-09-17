2d778c881329f7f4fc99365f27cff868c3974473
MyDell <1693625254@qq.com>
2021-09-17 21:21:31 +0800

    20210917

---
+++ src/com/leetcode/T24.java
@@ -0,0 +1,40 @@
+package com.leetcode;
+
+/**
+ * @author Enbing
+ * @create 2021-09-17 9:11 PM
+ * @Description
+ */
+public class T24 {
+
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = head.next;
+        head.next = swapPairs(newHead.next);
+        newHead.next = head;
+        return newHead;
+
+    }
+
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
+
