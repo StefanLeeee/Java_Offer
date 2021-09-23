d01ce0dc1d1b1d3e2820dfbc9a9d6f59ffa2c386
MyDell <1693625254@qq.com>
2021-09-23 22:48:29 +0800

    20210923

---
+++ src/com/leetcode/T100.java
@@ -0,0 +1,54 @@
+package com.leetcode;
+
+/**
+ * @author Enbing
+ * @create 2021-09-23 9:54 PM
+ * @Description
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+
+public class T100 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {
+        }
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        } else if (p.val == q.val) {
+            return true;
+        } else {
+            return isSameTree(p.left, q.left) && isSameTree(p.left, q.right);
+        }
+
+    }
+
+}
